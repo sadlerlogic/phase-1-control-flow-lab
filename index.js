b03178519a7811443fb639b8c43d7b827c0efca3
Christopher <sadlerlogic@gmail.com>
2022-02-09 19:52:03 -0600

    commit tree 24ef8377fddd94cb6f809a7d7e812d8e6f5ff98a

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(free){
+  if (free <= "400"){
+ return scuberGreetingForFeet = 'This one is on me!';
+   }else if(free >= "2001" && free < 2500){
+    return scuberGreetingForFeet = 'I will gladly take your thirty bucks.';
+  }else {
+    return scuberGreetingForFeet = 'No can do.';
+          }
+          scuberGreetingForFeet;
+  }
+
+function ternaryCheckCity(whereTo){
   // Write your code here!
+return whereTo === 'NYC' ?  'Ok, sounds good.' :'No go.'; 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+
+
+function switchOnCharmFromTip(gimmieTip){
   // Write your code here!
-}
+  if (gimmieTip === 'generous'){
+    return switchOnCharmFromTip = 'Thank you so much.';
+  }else if(gimmieTip === 'not as generous' ){
+       return switchOnCharmFromTip = 'Thank you.';
+  }else {
+    return switchOnCharmFromTip = 'Bye.';
+  }
+
+  switchOnCharmFromTip;
+  }
